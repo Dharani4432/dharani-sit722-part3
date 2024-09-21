77ee6e3e5c19ecc877f6bece3e7a5f6fb8d74ec3
Dharani Nuthula <dharanireddy952@gmail.com>
2024-09-22 08:58:10 +1000

    initial commit

--- book_catalog/app/config.py
+++ book_catalog/app/config.py
@@ -1,3 +1,3 @@
 import os
 
-DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://user:password@host:port/dbname')
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql://dharani722w7_user:RaOLEb1BZrCeFLgknSxKfKX8DeOERxWs@dpg-crnk5el6l47c73ahk390-a.oregon-postgres.render.com/dharani722w7')
